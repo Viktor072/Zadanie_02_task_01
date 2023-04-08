d82f5a3c363c60cfd765abd3a84f55532e263217
Viktor <rodkiv@mail.ru>
2023-04-08 14:06:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice;
+        ticketPrice = 13_676;
+        int milesPrice;
+        milesPrice = 20;
+        int bonusMiles = ticketPrice / milesPrice;
+        System.out.println(bonusMiles);
+    }
+}
+
